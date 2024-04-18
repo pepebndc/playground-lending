2c93fff16be43e5c8de61ff0d345732b521902a8
Pepe Blasco <pepeblascondc@gmail.com>
2024-04-18 18:56:18 +0200

    testing

---
+++ app/not-found.tsx
@@ -0,0 +1,54 @@
+import Link from "next/link";
+import Header from "./components/Header";
+export default function NotFound() {
+  return (
+    <div>
+      <Header />
+      <div className="relative isolate px-6 pt-14 lg:px-8">
+        <div
+          className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+          aria-hidden="true"
+        >
+          <div
+            className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff0000] to-[#aa2222] opacity-70 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+            style={{
+              clipPath:
+                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+            }}
+          />
+        </div>
+
+        <div className="h-screen flex items-center justify-center">
+          <div className="text-center justify-items-center align-middle h-auto">
+            <h1 className="mb-4 text-6xl font-semibold text-red-500">404</h1>
+            <p className="mb-4 text-lg text-gray-600">
+              Oops! Looks like you're lost.
+            </p>
+            <div className="animate-bounce">
+              <svg
+                className="mx-auto h-16 w-16 text-red-500"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"
+                ></path>
+              </svg>
+            </div>
+            <p className="mt-4 text-gray-600">
+              Let's get you back{" "}
+              <a href="/" className="text-blue-500">
+                home
+              </a>
+              .
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
-import Image from "next/image";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Feature from "./components/Feature";
 
 export default function Home() {
   return (
     <main>
-      HOLA!
-
-      
-
+      <Header />
+      <Hero />
+      <Feature />
     </main>
   );
 }
